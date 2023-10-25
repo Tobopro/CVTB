d0f81318c468a63e03f411ba5cbfc2baa01c6e6b
Tobopro <145339180+Tobopro@users.noreply.github.com>
2023-10-25 11:25:03 +0200

    v3 navTopButton and offcanvas+caroussel

--- monscript.js
+++ monscript.js
@@ -16,10 +16,19 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
         darkSubtleElements.forEach(element => {
             element.classList.remove('bg-dark-subtle');
             element.classList.add('bg-light');
+        })
+      
+        const whiteTextElements = document.querySelectorAll('.text-white');
+        whiteTextElements.forEach(element => {
+            
+            element.classList.remove('text-white');
+            element.classList.add('text-secondary');
         });
+        
         // Affichez l'icône du soleil et masquez l'icône de la lune
-        sunIcon.classList.remove('d-none');
-        moonIcon.classList.add('d-none');
+        moonIcon.classList.remove('d-none');
+        sunIcon.classList.add('d-none');
+        
     } else {
         htmlElement.setAttribute('data-bs-theme', 'dark');
         // Changez les éléments avec la classe "bg-light" en "bg-dark-subtle"
@@ -27,9 +36,37 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
         lightElements.forEach(element => {
             element.classList.remove('bg-light');
             element.classList.add('bg-dark-subtle');
+        })
+        const secondaryTextElements = document.querySelectorAll('.text-secondary');
+        secondaryTextElements.forEach(element => {
+            element.classList.remove('text-secondary');
+            element.classList.add('text-white');
+   
+            
         });
+
         // Affichez l'icône de la lune et masquez l'icône du soleil
-        moonIcon.classList.remove('d-none');
-        sunIcon.classList.add('d-none');
+        sunIcon.classList.remove('d-none');
+        moonIcon.classList.add('d-none');
     }
 });
+
+// Get the button:
+let mybutton = document.getElementById("navTopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.classList.remove("d-none");
+  } else {
+    mybutton.classList.add("d-none");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
